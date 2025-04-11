4f21dd327d4b36d71920b024ea7d629ae769b252
annamayppobeda <163832728+annamayppobeda@users.noreply.github.com>
2025-04-11 11:46:38 +0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,4 +2,4 @@ from app import calculate_area_of_parallelogram
 
 
 def test_calculate_area_of_parallelogram():
-    assert calculate_area_of_parallelogram(4, 5) ==20
+    assert calculate_area_of_parallelogram(4, 5) == 20
